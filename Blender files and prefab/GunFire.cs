c229adc30e0f97185489742b9167fb71be0202ec
Lunaris Knight <56858945+darkslade9@users.noreply.github.com>
2020-02-23 16:20:23 +0530

    Add files via upload

---
+++ Blender files and prefab/GunFire.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunFire : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public float fireRate = 15f;
+
+    public Camera fpsCam;
+    public GameObject impactEffect;
+
+    private float nextTimeToFire = 0f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Mouse0) && Time.time >= nextTimeToFire)
+        {
+            // GetComponent<AudioSource>().Play();
+            Shoot();
+            GetComponent<Animation>().Play();
+            nextTimeToFire = Time.time + 1f / fireRate;
+        }
+    }
+    void Shoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCam.transform.position,fpsCam.transform.forward,out hit,range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if(target != null)
+            {
+                target.TakeDamage(damage);
+            }
+            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, .5f);
+        }
+    }
+}
